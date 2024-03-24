2e77bd7a7fecc3f40a72c6c89d4bef004a7397b7
hemu07 <90203539+hemu07@users.noreply.github.com>
2024-03-25 01:07:54 +0530

    Add files via upload
    Script to monitor AWS resources

---
+++ aws_resource_monitor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+###############################################################
+
+#Author: Hemali Jayswal
+#Date: 24th March
+#script to monitor & report the AWS resources
+#Resources monitored:AWS s3, AWS EC2, AWS Lambda, AWS IAM users
+
+###############################################################
+
+#set -x # debug mode for runnning script
+
+# List S3 buckets
+echo " list s3 buckets: "
+aws s3 ls
+
+#List ec2 instances
+echo " list ec2 instances: "
+aws ec2 describe-instances
+
+#List aws Lamda functions
+echo "list lambda functions: "
+aws lambda list-functions
+
+#List IAM users
+echo "list IAM users: "
+aws iam list-users | jq '.Users[].UserId'
+
+
