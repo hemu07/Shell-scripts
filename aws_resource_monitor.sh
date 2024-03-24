0b64f3b6fa60f9ecd22a2f2b3c269a4f231875a1
hemu07 <90203539+hemu07@users.noreply.github.com>
2024-03-25 01:45:12 +0530

    Update aws_resource_monitor.sh
    modified to get simplified outout from json

--- aws_resource_monitor.sh
+++ aws_resource_monitor.sh
@@ -10,20 +10,16 @@
 
 #set -x # debug mode for running script
 
-# List S3 buckets
-echo " list s3 buckets: "
-aws s3 ls
-
-#List ec2 instances
-echo " list ec2 instances: "
-aws ec2 describe-instances
-
-#List aws Lamda functions
-echo "list lambda functions: "
-aws lambda list-functions
-
-#List IAM users
 echo "list IAM users: "
 aws iam list-users | jq '.Users[].UserId'
+
+echo "list ec2 instances: "
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+echo "list s3 buckets: "
+aws s3 ls
+
+echo "list lambda functions: "
+aws lambda list-functions | jq '.Functions'
 
 
